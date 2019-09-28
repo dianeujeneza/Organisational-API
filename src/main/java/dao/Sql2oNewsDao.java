fb4f9522ed07109650f85a290d63d1fa97f571cd
Diane Ujeneza <dianesis17@gmail.com>
2019-09-28 20:17:48 +0200

    newsdao properties

--- src/main/java/dao/Sql2oNewsDao.java
+++ src/main/java/dao/Sql2oNewsDao.java
@@ -1,5 +1,82 @@
 package dao;
 
-public class Sql2oNewsDao {
+import models.Department;
+import models.News;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
+import java.util.List;
+
+public class Sql2oNewsDao implements NewsDao{
+
+    private final Sql2o sql2o;
+    public Sql2oNewsDao(Sql2o sql2o){ this.sql2o = sql2o; }
+
+    @Override
+    public void add(News news) {
+        String sql = "INSERT INTO news (name) VALUES (:name)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(news)
+                    .executeUpdate()
+                    .getKey();
+            news.setNewsId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public void addNewsToDepartment(News news, Department department) {
+        String sql = "INSERT INTO department_news (departmentId, newsId) VALUES (:departmentId, :newsId)";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("departmentId", department.getId())
+                    .addParameter("newsId", news.getNewsId())
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public List<News> getAll() {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM news")
+                    .executeAndFetch(News.class);
+        }
+    }
+
+    @Override
+    public News findById(int id) {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM news WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(News.class);
+        }
+    }
+
+    @Override
+    public List<Department> getAllDepartmentNews(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+        String sql = "DELETE from news";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -5,7 +5,7 @@ import models.User;
 import java.util.List;
 
 public interface UserDao {
-    void add (User user);
+    void add(User user);
 
     List<User>getAll();
     List<User>getAllUserByDepartment(int departmentId);
