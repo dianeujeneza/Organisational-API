46c27b58a987158a60deb7b16a687b806d33a068
Diane Ujeneza <dianesis17@gmail.com>
2019-09-28 19:22:20 +0200

    user and news interface

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,17 @@
 package dao;
 
+import models.Department;
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    void add (News news);
+    void addNewsToDepartment(News news, Department department);
+
+    List<News>getAll();
+    News findById(int id);
+    List<Department>getAllDepartmentNews(int id);
+
+    void deleteById(int id);
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,5 +1,14 @@
 package dao;
 
-public interface UserDao {
+import models.User;
 
+import java.util.List;
+
+public interface UserDao {
+void add (User user);
+
+List<User>getAll();
+List<User>getAllUserByDepartment(int departmentId);
+
+void deleteById(int id);
 }
