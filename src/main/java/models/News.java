bb4db1c8b84b167624c53c7af077fdac6f0204e8
Diane Ujeneza <dianesis17@gmail.com>
2019-09-28 18:58:09 +0200

    news properties

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,6 +1,39 @@
 package models;
 
+import java.util.Objects;
+
 public class News {
+
 private int newsId;
 private String depNews;
+
+    public int getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(int newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getDepNews() {
+        return depNews;
+    }
+
+    public void setDepNews(String depNews) {
+        this.depNews = depNews;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof News)) return false;
+        News news = (News) o;
+        return getNewsId() == news.getNewsId() &&
+                getDepNews().equals(news.getDepNews());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNewsId(), getDepNews());
+    }
 }
