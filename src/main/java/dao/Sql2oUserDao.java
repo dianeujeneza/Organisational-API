a0fe239ba523876e861f3c87caaf94e91ca5e5b0
Diane Ujeneza <dianesis17@gmail.com>
2019-09-28 19:45:28 +0200

    dependencies

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -1,5 +1,14 @@
 package dao;
 
+import models.User;
+
+import java.sql.Connection;
+
 public class Sql2oUserDao {
 
+//    @Override
+    public void add(User user){
+        try(Connection con=)
+
+    }
 }
